d34687262a4b410844f14f99ab2e957b3015a0fa
Oratile Ame Busang <oratile@inq.inc>
2024-07-26 11:48:36 +0200

    Prep for deployment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import AboutMe from './components/about';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to Ame's Portfolio</h1>
+        <p>This is my developer portfolio</p>
       </header>
+      <AboutMe />
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutMe() {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p>Hello! I'm a passionate developer with experience in building web applications.</p>
+    </section>
+  );
+}
+
+export default AboutMe;
